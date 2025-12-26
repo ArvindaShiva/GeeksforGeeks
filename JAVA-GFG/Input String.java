2eb30f02bffc46457a7e21d86e9f0fce5794373f
Arvinda Shiva R G <rgarvindashiva@gmail.com>
2025-12-26 20:26:17 +0530

    Add files via upload

---
+++ JAVA-GFG/Input String.java
@@ -0,0 +1,21 @@
+/*Given a text in the form of a string, you need to take input and print it.
+
+Examples:
+
+Input: s = "abcd"
+Output: "abcd"
+Explanation: The input is printed as it is.
+*/
+
+//Code:
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.print(s);
+    }
+}
